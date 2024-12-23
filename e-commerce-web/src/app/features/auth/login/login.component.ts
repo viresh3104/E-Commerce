c40a0be59e33af28d88dbd64c169e8c18db1a0f7
viresh3104 <vireshnavtake3104@gmail.com>
2024-12-23 11:38:20 +0530

    added the login page structure with html and scss

--- e-commerce-web/src/app/features/auth/login/login.component.ts
+++ e-commerce-web/src/app/features/auth/login/login.component.ts
@@ -1,22 +1,59 @@
+// import { Component } from '@angular/core';
+// import { Router } from '@angular/router';
+
+// @Component({
+//   selector: 'app-login',
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.scss']
+// })
+// export class LoginComponent {
+//   constructor(private router: Router) {}
+
+//   onLogin(): void {
+//     // Add your authentication logic here
+//     const isAuthenticated = true; // Replace with actual authentication result
+
+//     if (isAuthenticated) {
+//       this.router.navigate(['/dashboard']);
+//     } else {
+//       alert('Invalid credentials');
+//     }
+//   }
+// }
+
 import { Component } from '@angular/core';
+// import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  email = '';
+  password = '';
+  constructor(private router: Router , private http : HttpClient) {}
 
-  onLogin(): void {
-    // Add your authentication logic here
-    const isAuthenticated = true; // Replace with actual authentication result
+  ngOnInit(): void {}
 
-    if (isAuthenticated) {
-      this.router.navigate(['/dashboard']);
-    } else {
-      alert('Invalid credentials');
-    }
+
+  login(){
+    const loginData = {
+      email: this.email,
+      password: this.password,
+    };
+
+    this.http.post('http://localhost:5000/api/auth/login',loginData).subscribe({
+      
+
+    })
+
+  }
+
+  register() {
+    this.router.navigate(['register']);
   }
 }
