b03e01d10eaf0e07bfa8f1b1871aafb0b07c3f58
viresh3104 <vireshnavtake3104@gmail.com>
2025-04-23 12:48:45 +0530

    modification nedded for fonts and icons from font awesom

--- e-commerce-web/src/app/app-routing.module.ts
+++ e-commerce-web/src/app/app-routing.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
-  { path: 'auth', loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule) },
-  { path: 'dashboard', loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule) },
-  { path: '', redirectTo: 'auth/login', pathMatch: 'full' }, // Default route
+  { path: '', redirectTo: 'auth/register', pathMatch: 'full' },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./main/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'user',
+    loadChildren: () =>
+      import('./main/user/user.module').then((m) => m.UserModule),
+    canActivate: [authGuard],
+  },
   { path: '**', redirectTo: 'auth/login' }, // Wildcard route
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- e-commerce-web/src/app/app.module.ts
+++ e-commerce-web/src/app/app.module.ts
@@ -3,23 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './layouts/header/header.component';
-import { FooterComponent } from './layouts/footer/footer.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatSnackBarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
